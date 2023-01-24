fabbf627e45f93d25162794376e1f65ff707d0ec
Shoaib <shoaibakbarm@gmail.com>
2023-01-25 01:50:48 +0500

    appling styling

--- simple-react-calculator/src/App.jsx
+++ simple-react-calculator/src/App.jsx
@@ -1,5 +1,33 @@
+import "./styles.css";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="main-container">
+      <div className="screen-container">
+        <div className="previous-operands">1122+</div>
+        <div className="current-operands">53636</div>
+      </div>
+
+      <button className="span-2">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>×</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>-</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>+</button>
+      <button>0</button>
+      <button>.</button>
+      <button className="span-2">=</button>
+    </div>
+  );
 }
 
 export default App;
